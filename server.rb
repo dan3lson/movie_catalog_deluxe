557c4a229ad02c38596def458f6b9b2a1ed13c7e
Danelson Rosa <danelson.rosa@gmail.com>
2015-05-28 18:45:32 -0400

    Actors retrieved from the db as well as Movies titles

--- server.rb
+++ server.rb
@@ -2,22 +2,42 @@ require "sinatra"
 require "pg"
 require "pry"
 
+def db_connection
+  begin
+    connection = PG.connect(dbname: "movies")
+    yield(connection)
+  ensure
+    connection.close
+  end
+end
+
+def sql(statement)
+  all = db_connection do |conn|
+    conn.exec(statement)
+  end
+  all = all.to_a
+end
+
 get "/" do
   erb :index, locals: {}
 end
 
 get "/movies" do
-  erb :"/movies/index", locals: {}
+  movies = sql("SELECT * FROM movies ORDER BY title LIMIT 25")
+  erb :"/movies/index", locals: { movies: movies }
 end
 
 get "/movies/:movie" do
-  erb :"/movies/show", locals: {}
+  movie = params[:movie]
+  erb :"/movies/show", locals: { movie: movie }
 end
 
 get "/actors" do
-  erb :"/actors/index", locals: {}
+  actors = sql("SELECT * FROM actors ORDER BY name DESC LIMIT 25")
+  erb :"/actors/index", locals: { actors: actors }
 end
 
 get "/actors/:actor" do
-  erb :"/actors/show", locals: {}
+  actor = params[:actor]
+  erb :"/actors/show", locals: { actor: actor }
 end
