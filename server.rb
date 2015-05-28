60b48f5847232c28dbc44338903d6eafe0424f5c
Danelson Rosa <danelson.rosa@gmail.com>
2015-05-28 16:38:45 -0400

    Set up movies and actors routes and working navigation.

---
+++ server.rb
@@ -0,0 +1,23 @@
+require "sinatra"
+require "pg"
+require "pry"
+
+get "/" do
+  erb :index, locals: {}, layout: false
+end
+
+get "/movies" do
+  erb :"/movies/index", locals: {}
+end
+
+get "/movies/:movie" do
+  erb :"/movies/show", locals: {}
+end
+
+get "/actors" do
+  erb :"/actors/index", locals: {}
+end
+
+get "/actors/:actor" do
+  erb :"/actors/show", locals: {}
+end
