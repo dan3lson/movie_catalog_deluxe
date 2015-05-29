915b94a8ada19f30532e7a246d5261b8f54e6aea
Danelson Rosa <danelson.rosa@gmail.com>
2015-05-29 00:25:49 -0400

    Completed core reqs. Now able to view movie and actor details

--- server.rb
+++ server.rb
@@ -18,26 +18,85 @@ def sql(statement)
   all = all.to_a
 end
 
+def movies_info
+# why doesn't it work if studio and genre are swapped?
+  sql("
+    SELECT
+      movies.id,
+      movies.title,
+      movies.year,
+      movies.rating,
+      studios.name AS studio,
+      genres.name AS genre
+    FROM movies
+    JOIN genres ON movies.genre_id = genres.id
+    JOIN studios ON movies.studio_id = studios.id
+    ORDER BY title
+    LIMIT 100
+  ")
+end
+
 get "/" do
   erb :index, locals: {}
 end
 
 get "/movies" do
-  movies = sql("SELECT * FROM movies ORDER BY title LIMIT 25")
-  erb :"/movies/index", locals: { movies: movies }
+  erb :"/movies/index", locals: { movies: movies_info }
 end
 
+def movie_actors(movie_id)
+  sql("
+    SELECT actors.name AS actor, actors.id, cast_members.character
+    FROM cast_members
+    JOIN actors ON actors.id = cast_members.actor_id
+    JOIN movies ON cast_members.movie_id = movies.id
+    WHERE movies.id = '#{movie_id}'
+  ")
+end
 get "/movies/:movie" do
-  movie = params[:movie]
-  erb :"/movies/show", locals: { movie: movie }
+  movie_id = params[:movie]
+  movies_details = sql("
+    SELECT
+      movies.id,
+      movies.title,
+      movies.year,
+      movies.rating,
+      studios.name AS studio,
+      genres.name AS genre
+    FROM movies
+    JOIN genres ON movies.genre_id = genres.id
+    JOIN studios ON movies.studio_id = studios.id
+    WHERE movies.id = '#{movie_id}'
+    ORDER BY title
+    LIMIT 100
+  ")
+  erb :"/movies/show", locals: {
+    movies_details: movies_details,
+    movies_cast: movie_actors(movie_id)
+  }
 end
 
 get "/actors" do
-  actors = sql("SELECT * FROM actors ORDER BY name DESC LIMIT 25")
+  actors = sql("SELECT * FROM actors ORDER BY name LIMIT 100")
   erb :"/actors/index", locals: { actors: actors }
 end
 
-get "/actors/:actor" do
-  actor = params[:actor]
-  erb :"/actors/show", locals: { actor: actor }
+get "/actors/:id" do
+  actor_id = params[:id]
+  performances = sql("
+    SELECT
+      actors.name,
+      movies.id,
+      movies.title,
+      cast_members.character
+    FROM cast_members
+    JOIN movies ON cast_members.movie_id = movies.id
+    JOIN actors ON cast_members.actor_id = actors.id
+    WHERE actors.id = '#{actor_id}'
+  ")
+
+  erb :"/actors/show", locals: {
+    actor_id: actor_id,
+    performances: performances
+  }
 end
